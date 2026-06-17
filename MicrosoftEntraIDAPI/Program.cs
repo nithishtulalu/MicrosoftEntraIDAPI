d10a9d9e5923c15456959829bcbd6f1f341c0cf4
tulalunithish <nithishtulalu121@gmail.com>
2026-06-17 17:30:01 +0530

    Add CORS policy for Angular frontend

--- MicrosoftEntraIDAPI/Program.cs
+++ MicrosoftEntraIDAPI/Program.cs
@@ -39,11 +39,23 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                         builder.Configuration["Jwt:Key"]))
     };
 });
+
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("AngularPolicy",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseCors("AngularPolicy");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
