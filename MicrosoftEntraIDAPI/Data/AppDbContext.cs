5367fee8a34be8dc975461c7cac9404c3503cc75
tulalunithish <nithishtulalu121@gmail.com>
2026-06-17 12:23:20 +0530

    Add Entity Framework Core setup, AutoMapper, and FluentValidation for user management

---
+++ MicrosoftEntraIDAPI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MicrosoftEntraIDAPI.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ MicrosoftEntraIDAPI/Validation/RegisterValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace MicrosoftEntraIDAPI.Validation
+{
+    public class RegisterValidator: AbstractValidator<RegisterDto>
+    {
+        public RegisterValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty();
+            RuleFor(x => x.LastName)
+                .NotEmpty();
+            RuleFor(x => x.Email)
+                .EmailAddress()
+                .NotEmpty();
+            RuleFor(x => x.Password)
+                .MinimumLength(6)
+                .NotEmpty();
+            RuleFor(x => x.PhoneNumber)
+                .NotEmpty();
+        }
+    }
+}
